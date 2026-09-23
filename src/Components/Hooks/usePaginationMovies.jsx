04a51e91a7eb13f25d4c3b97dfd52d1c6e92b706
Ioan Tudor <147550104+ioanTudo@users.noreply.github.com>
2026-09-23 13:07:39 +0300

    fix: simplify pagination state hook

--- src/Components/Hooks/usePaginationMovies.jsx
+++ src/Components/Hooks/usePaginationMovies.jsx
@@ -1,78 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const usePaginationMovies = () => {
-  const [list, setList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [topRatedCurrentPage, setTopRatedCurrentPage] = useState(1);
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const url = `https://api.themoviedb.org/3/discover/movie?page=${currentPage}`;
-        const options = {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjE0YzM1YzU0ZDJjZDM4Yzk0NjkwY2UzMDI3MDk0ZSIsIm5iZiI6MTczODA5MzczMy41MjcsInN1YiI6IjY3OTkzNGE1MWJlMTE2NDA5YzIzODk4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nTebm3iPBrMjCJcgW-ZUykU1iF95u99wfUTXy5g9Y4M",
-          },
-        };
-
-        const response = await fetch(url, options);
-        const data = await response.json();
-
-        if (!data.results) {
-          throw new Error("No results found");
-        }
-
-        setList(() => [...list, ...data.results.slice(0, 14)]);
-      } catch (error) {}
-    };
-
-    fetchMovies();
-  }, [currentPage]);
-
-  useEffect(() => {
-    const fetchTopRatedMovies = async () => {
-      try {
-        const url = `https://api.themoviedb.org/3/movie/top_rated?page=${topRatedCurrentPage}`;
-        const options = {
-          method: "GET",
-          headers: {
-            accept: "application/json",
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjE0YzM1YzU0ZDJjZDM4Yzk0NjkwY2UzMDI3MDk0ZSIsIm5iZiI6MTczODA5MzczMy41MjcsInN1YiI6IjY3OTkzNGE1MWJlMTE2NDA5YzIzODk4ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nTebm3iPBrMjCJcgW-ZUykU1iF95u99wfUTXy5g9Y4M",
-          },
-        };
-
-        const response = await fetch(url, options);
-        const data = await response.json();
-
-        if (!data.results) {
-          throw new Error("No top-rated movies found");
-        }
-
-        setTopRatedMovies(() => [...topRatedMovies, ...data.results]);
-      } catch (error) {}
-    };
-
-    fetchTopRatedMovies();
-  }, [topRatedCurrentPage]);
-
   const loadMore = () => {
-    setCurrentPage(() => currentPage + 1);
+    setCurrentPage((page) => page + 1);
   };
 
   const loadMoreTopRated = () => {
-    setTopRatedCurrentPage(() => topRatedCurrentPage + 1);
+    setTopRatedCurrentPage((page) => page + 1);
   };
 
   return {
-    list,
     currentPage,
     loadMore,
-    topRatedMovies,
     topRatedCurrentPage,
     loadMoreTopRated,
   };
